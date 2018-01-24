be03eb3b77064c15e8c0176ee11da5b504cbf8e1
Andy Slezak <andy@cytexone.com>
2018-01-24 17:18:10 -0500

    Style Portfolio Component on Home Page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,41 +5,56 @@ import Img from "gatsby-image";
 import styled from "react-emotion";
 import { ThemeProvider } from "emotion-theming";
 
-import { Flex, Box, Heading, NavLink, Container, Absolute } from "rebass";
+import { Flex, Heading, Box, Container, Absolute } from "rebass";
 import XRay from "react-x-ray";
 import theme from "../css/Theme";
 import "../css/aslezak.scss";
 
+import HeaderLight from "../styled/HeaderLight";
+import SubHeaderLight from "../styled/SubHeaderLight";
+import LinkFancyLight from "../styled/LinkFancyLight";
+
 export default class App extends React.Component {
   render() {
     return (
-      <Flex>
-        <Box w={1}>
-          <Absolute z={-1} w={1}>
-            <Img className="portfolio-bg" sizes={this.props.background.sizes} />
-          </Absolute>
+      <ThemeProvider theme={theme}>
+        <Flex>
+          <Box w={1}>
+            <Absolute z={-1} w={1}>
+              <Img
+                className="portfolio-bg"
+                sizes={this.props.background.sizes}
+              />
+            </Absolute>
 
-          <Container align="center">
-            <Box w={1} py={2}>
-              <Heading align="left">
-                <Heading f={6}>WORK</Heading>
-                <Heading f={3}>Projects</Heading>
-              </Heading>
+            <Container align="center">
+              <Box w={1} py={2}>
+                <Heading align="left">
+                  <Flex column>
+                    <HeaderLight letterSpacing f={6}>
+                      WORK
+                    </HeaderLight>
+                    <SubHeaderLight letterSpacing={1} f={3}>
+                      Projects
+                    </SubHeaderLight>
+                  </Flex>
+                </Heading>
 
-              <Link to={"/portfolio"}>
-                <Img
-                  className="portfolio-image"
-                  sizes={this.props.image.sizes}
-                />
-              </Link>
+                <Link to={"/portfolio"}>
+                  <Img
+                    className="portfolio-image"
+                    sizes={this.props.image.sizes}
+                  />
+                </Link>
 
-              <NavLink>
-                <Link to={"/portfolio"}>PORTFOLIO</Link>
-              </NavLink>
-            </Box>
-          </Container>
-        </Box>
-      </Flex>
+                <LinkFancyLight f={[2, 3]} letterSpacing={2 * 0.75}>
+                  <Link to={"/portfolio"}>PORTFOLIO</Link>
+                </LinkFancyLight>
+              </Box>
+            </Container>
+          </Box>
+        </Flex>
+      </ThemeProvider>
     );
   }
 }
