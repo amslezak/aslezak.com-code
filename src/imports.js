a452ea90c5331b18946a2e63faa87811f3065e65
Andy Slezak <andy@cytexone.com>
2018-01-24 17:20:39 -0500

    Create Import Template to copy from

--- src/imports.js
+++ src/imports.js
@@ -7,11 +7,14 @@ import Img from "gatsby-image";
 // Emotion
 import styled from "react-emotion";
 import { ThemeProvider } from "emotion-theming";
+
+// aslezak.com
 import theme from "../css/Theme";
 import "../css/aslezak.scss";
 
 // Jxnblk
 import { Flex, Box, Heading, Text, Container, Border, NavLink } from "rebass";
-import { Grid } from "grid-styled";
 import { flex, space, width, fontSize, color } from "styled-system";
 import XRay from "react-x-ray";
+
+import { Grid } from "grid-styled";
