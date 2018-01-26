610e90623c90c5130d980698c4c6cfc629bd530b
Andy Slezak <andy@cytexone.com>
2018-01-26 14:14:46 -0500

    Make Hero Overview Responsive

--- src/components/HeroOverview.js
+++ src/components/HeroOverview.js
@@ -35,20 +35,20 @@ export default class HeroOverview extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <Flex bg="#fff" align="center">
+        <Flex bg="#fff" align="center" py={[2, 3, 2, 3]}>
           <Container>
-            <Box my={3}>
-              <HeaderDark letterSpacing={0.75} lineHeight={2} f={3}>
+            <Box my={[1, 3]}>
+              <HeaderDark letterSpacing={0.75} lineHeight={2} f={[3, 4]}>
                 {this.props.title}
               </HeaderDark>
-              <Text my={1} f={2}>
+              <Text my={1} f={[2, 3]}>
                 {this.props.body}
               </Text>
 
               <Flex>
-                <Box my={3}>
+                <Box my={[3, 3, 3, 3]}>
                   <Link to="more" smooth={true} duration={500} offset={-25}>
-                    <Button letterSpacing={1.25} borderRadius={4} p={2}>
+                    <Button letterSpacing={1.25} borderRadius={4} p={[2]}>
                       Learn More
                     </Button>
                   </Link>
