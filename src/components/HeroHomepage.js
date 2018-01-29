c36746c3f7046eb9b81b80afef309c1ef7aa8e7f
Andy Slezak <andy@cytexone.com>
2018-01-29 13:54:29 -0500

    Responsive Home Hero Section and text tweaking

--- src/components/HeroHomepage.js
+++ src/components/HeroHomepage.js
@@ -8,35 +8,67 @@ import theme from "../css/Theme";
 import { Flex, Box, Heading, Absolute, Container, Relative } from "rebass";
 import XRay from "react-x-ray";
 
+import HomeHeroHeaderLight1 from "../styled/HomeHeroHeaderLight1";
+import HomeHeroSubHeaderLight from "../styled/HomeHeroSubHeaderLight";
+import HomeHeroOverlay from "../styled/HomeHeroOverlay";
+
 export default class componentName extends Component {
   render() {
     return (
-      <Flex>
-        <Box w={1}>
-          <Absolute w={1} z={-1}>
-            <Img
-              className="homeHero-background"
-              sizes={this.props.background.sizes}
-              backgroundColor="#99765E"
-            />
-          </Absolute>
-          <Relative w={1}>
-            <Absolute w={1}>
-              <Container w={1} align="center" mt={3}>
-                <Heading pt={5} w={1}>
-                  <h3>Welcome! I'm</h3>
-                  <h1>Andy Slezak</h1>
-                  <h2>I design & code enjoyable web apps</h2>
-                </Heading>
-              </Container>
+      <ThemeProvider theme={theme}>
+        <Flex>
+          <Box w={1}>
+            <Absolute w={1} z={-1}>
+              <Img
+                className="homeHero-background"
+                sizes={this.props.background.sizes}
+                backgroundColor="#99765E"
+              />
             </Absolute>
-            <Img className="homeHero-gear" sizes={this.props.gear.sizes} />
-          </Relative>
-          <Container>
-            <Box w={1} />
-          </Container>
-        </Box>
-      </Flex>
+
+            <Relative w={1}>
+              <Absolute w={1} z={2}>
+                <Container w={1} align="center" mt={3}>
+                  <Box w={1}>
+                    <Flex column>
+                      <Heading pt={[0, 3, 3, 4]} w={1}>
+                        <HomeHeroHeaderLight1
+                          f={[5, 6, 6, 7]}
+                          letterSpacing={1}
+                          lineHeight={1 * 1.5}
+                        >
+                          <Box mb={[0, 0, 0, 0]}>Andy Slezak</Box>
+                        </HomeHeroHeaderLight1>
+                        <Box>
+                          <HomeHeroSubHeaderLight
+                            f={[4, 4, 4, 5]}
+                            letterSpacing={1}
+                            lineHeight={1 * 1.25}
+                          >
+                            <Box>design & code smart web apps</Box>
+                          </HomeHeroSubHeaderLight>
+                        </Box>
+                      </Heading>
+                    </Flex>
+                  </Box>
+                </Container>
+              </Absolute>
+              <HomeHeroOverlay w={1} z={1}>
+                <Relative z={-1}>
+                  <Img
+                    className="homeHero-gear"
+                    sizes={this.props.gear.sizes}
+                  />
+                </Relative>
+              </HomeHeroOverlay>
+              <Absolute />
+            </Relative>
+            <Container>
+              <Box w={1} />
+            </Container>
+          </Box>
+        </Flex>
+      </ThemeProvider>
     );
   }
 }
