355d2b3e7bbf19f8ecd3b40099207c57c5700b8a
Andy Slezak <andy.slezak@gmail.com>
2018-02-05 12:39:12 -0500

    Subfooter cleanup

--- src/components/FooterSub.js
+++ src/components/FooterSub.js
@@ -1,44 +1,61 @@
-import React from "react";
+import React from "react"
 
 // Gatsby
-import Link from "gatsby-link";
-import Img from "gatsby-image";
+import Link from "gatsby-link"
+import Img from "gatsby-image"
 
 // Emotion
-import styled from "react-emotion";
-import { ThemeProvider } from "emotion-theming";
-import theme from "../css/Theme";
+import styled from "react-emotion"
+import { ThemeProvider } from "emotion-theming"
+import theme from "../css/Theme"
 
 // Jxnblk
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
-import { Flex, Box, Heading, Text, Container, Border, NavLink } from "rebass";
-import { Grid } from "grid-styled";
-import XRay from "react-x-ray";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap"
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Container,
+  Border,
+  NavLink,
+  Small,
+} from "rebass"
+import { Grid } from "grid-styled"
+import XRay from "react-x-ray"
 
 export default class App extends React.Component {
   render() {
     return (
-      <Flex bg="#fff" column>
+      <Flex w={1} bg="#fff">
         <Container w={1} align="left">
-          <Box my={[0, 1, 1, 1]} py={2}>
-            <Grid w={75} mx={[0, 2, 2, 0]}>
-              <a href="https://twitter.com/aslezak">
-                <Img className="social-img" sizes={this.props.twitter.sizes} />
-              </a>
-            </Grid>
-            <Grid w={75} mx={[0, 2, 2, 2]}>
-              <a href="https://www.github.com/amslezak">
-                <Img className="social-img" sizes={this.props.github.sizes} />
-              </a>
-            </Grid>
-            <Grid w={75} mx={[0, 2, 2, 0]}>
-              <a href="https://www.codewars.com/users/aslezak">
-                <Img className="social-img" sizes={this.props.codewars.sizes} />
-              </a>
-            </Grid>
-          </Box>
+          <Flex align="center">
+            <Box w={1} my={[1]} py={1}>
+              <Grid w={75} mx={[0, 2, 2, 0]}>
+                <a href="https://twitter.com/aslezak">
+                  <Img
+                    className="social-img"
+                    sizes={this.props.twitter.sizes}
+                  />
+                </a>
+              </Grid>
+              <Grid w={75} mx={[0, 2, 2, 2]}>
+                <a href="https://www.github.com/amslezak">
+                  <Img className="social-img" sizes={this.props.github.sizes} />
+                </a>
+              </Grid>
+              <Grid w={75} mx={[0, 2, 2, 0]}>
+                <a href="https://www.codewars.com/users/aslezak">
+                  <Img
+                    className="social-img"
+                    sizes={this.props.codewars.sizes}
+                  />
+                </a>
+              </Grid>
+            </Box>
+          </Flex>
         </Container>
       </Flex>
-    );
+    )
   }
 }
