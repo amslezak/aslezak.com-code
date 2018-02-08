3acbae58bdca3104408a069156a77466938bf5ce
Andy Slezak <andy.slezak@gmail.com>
2018-02-08 12:15:12 -0500

    remove workaround since it was fixed in gatsbyjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,15 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
-
 import objectFitImages from "object-fit-images"
 
 exports.onClientEntry = () => {
   objectFitImages()
-}
-
-exports.shouldUpdateScroll = (prevRouterProps, pathname) => {
-  window.scrollTo(0, 0)
-  return false
 }
